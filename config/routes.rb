b02b1e60175e8165cba63d1b59d083ae5686c873
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-14 09:10:24 +0000

    namespaceの勉強

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'items' => 'public/items#index'
   namespace :admin do
-    get 'items' => 'admin/items#index'
+    resources :items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
